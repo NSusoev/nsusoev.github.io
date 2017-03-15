5de248fb57fd97ec54e5e12a8e24aa024e1d72d9
NSusoev <susoev.nikita@gmail.com>
2017-03-15 15:30:33 +0300

    #1 Backbone was created

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'),
+    browserSync = require('browser-sync'),
+    sass = require('gulp-sass');
+    csso = require('gulp-csso');
+
+gulp.task('sass', function () {
+  gulp.src('sass/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .on('error', console.log)
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('build', ['sass']);
+
+gulp.task('watch', ['build'], function() {
+  browserSync.init({
+    server: "."
+  });
+
+  gulp.watch('js/*.js', ['js']).on('change', browserSync.reload);
+  gulp.watch('./**/*.html').on('change', browserSync.reload);
+  gulp.watch('sass/*.sass', ['sass']).on('change', browserSync.reload);
+});
+
+gulp.task('default', ['watch']);
