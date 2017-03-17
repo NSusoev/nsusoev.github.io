73b50a7141b358c1fce1fd43a1eca22a441b1831
NSusoev <susoev.nikita@gmail.com>
2017-03-17 16:29:42 +0300

    #3 The initial outline of the code which draw the graph

--- js/main.js
+++ js/main.js
@@ -8,39 +8,47 @@ var nodeAreaHeight = 100;
 var nodesCount = Math.floor((paperWidth / nodeAreaWidth) * (paperHeight / nodeAreaHeight));
 var paper = Raphael("top_background", $("#top_background").width(), $("#top_background").height());
 
+var points = [];
 var nodes = [];
 
 for (var i = 0; i < nodesCount; i++) {
   var newX = getRandomInt(nodeRadius, paperWidth);
   var newY = getRandomInt(nodeRadius, paperHeight);
-
-  var circle = paper.circle(newX, newY, 5, 5);
-  circle.attr("fill", "#05f");
-  circle.attr("stroke", "#fff");
-  circle.attr("stroke-width", 1);
-  circle.attr("cursor", "pointer");
-  nodes.push(circle);
+  points.push({newX: newX , newY: newY});
 }
 
-console.log(nodes);
-var pathString = "";
-for (var i = 0; i < nodes.length; i++) {
-
-  if (i == 0) {
-    pathString += "M " + nodes[i].attr("cx") + " " + nodes[i].attr("cy") + " ";
+// TODO: sort
+points.sort(function(a, b) {
+  if (a.newX < b.newX) {
+    return -1;
   } else {
-    pathString += "L " + nodes[i].attr("cx") + " " + nodes[i].attr("cy") + " ";
+    return 1;
   }
 
-  if (i == nodes.length - 1) {
-    pathString += "Z";
+  return 0;
+});
+
+// draw
+var pathString = "";
+for (var i = 0; i < points.length; i++) {
+
+  if (i == 0) {
+    pathString += "M " + points[i].newX + " " + points[i].newY + " ";
+  } else {
+    pathString += "L " + points[i].newX + " " + points[i].newY + " ";
   }
 
   var path = paper.path(pathString);
   path.attr("stroke", "#fff");
-  path.attr("stroke-width", 0.5);
+  path.attr("stroke-width", 1);
 }
 
-console.log(pathString);
-
+for (var i = 0; i < points.length; i++) {
+  var circle = paper.circle(points[i].newX, points[i].newY, 7, 7);
+  circle.attr("fill", "#05f");
+  circle.attr("stroke", "#fff");
+  circle.attr("stroke-width", 2);
+  circle.attr("cursor", "pointer");
+  nodes.push(circle);
+}
 
